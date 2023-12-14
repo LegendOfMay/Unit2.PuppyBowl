b3b2412604421919a6f26755acbc18a1beaf9343
Joshua Dogma <joshuadogma@Joshuas-MacBook-Pro.local>
2023-12-14 15:21:49 -0500

    details page render upon click, form is also added to add a player to state object not API. CSS added to entire site

--- index.js
+++ index.js
@@ -16,25 +16,115 @@ renderPlayers()
 }
 getAllPlayers();
 
-//Render players to page 
+
+
+
+//Render players to  home page 
 
 const renderPlayers = () => {
     const main = document.getElementById("playerContainer");
     main.innerHTML = "";
 
-    state.allPlayers.forEach((puppy) => {
+
+    state.allPlayers.slice(0, 20).forEach((puppy) => {
         const playerDiv = document.createElement("div");
        
         playerDiv.innerHTML = `
             <img src="${puppy.imageUrl}" alt="${puppy.name}">
             <h3>${puppy.name}</h3>
             <p>Status: ${puppy.status}</p>
-            <button onclick="showPlayerDetails('${puppy.id}')">See Details</button>
+            <button data-player-id="${puppy.id}">See Details</button>
         `;
 
         main.appendChild(playerDiv);
     });
+    main.addEventListener("click", (event) => {
+        if (event.target.tagName === "BUTTON") {
+            const playerId = event.target.getAttribute("data-player-id");
+            showPlayerDetails(playerId);
+        }
+})};
+
+
+// Show PlayerDetails function (onClick)
+const showPlayerDetails = async (playerId) => {
+    console.log(`Showing details for player ID: ${playerId}`);
+    const main = document.getElementById("playerContainer");
+    main.innerHTML = "";
+
+
+    const response = await fetch(`${apiBaseURL}/${playerId}`);
+    const playerDetails = await response.json();
+    renderPlayerDetails(playerDetails.data.player);
+
 };
+
+// Render player details function to pass to (showPlayerDetails)
+
+const renderPlayerDetails = (player) => {
+
+    const playerDetailsContainer = document.getElementById("playerDetailsModal");
+    playerDetailsContainer.innerHTML = "";
+
+    const detailsContainer = document.createElement("div");
+    
+    // Add player details to the container
+    detailsContainer.innerHTML = `
+        <img src="${player.imageUrl}" alt="${player.name}">
+        <h2>${player.name}</h2>
+        <p>Status: ${player.status}</p>
+        <p>Breed: ${player.breed}</p>
+        <p>Age: ${player.age}</p>
+
+        <button onclick="closePlayerDetails()">Close</button>
+    `;
+
+    playerDetailsContainer.appendChild(detailsContainer);
+    playerDetailsContainer.style.display = "block";
+};
+
+
+// Function to close player details
+const closePlayerDetails = () => {
+    const playerDetailsContainer = document.getElementById("playerDetailsModal");
+    playerDetailsContainer.style.display = "none";
+    renderPlayers();
+};
+
+// Function to add players via form and (preventDefault)
+
+document.getElementById("playerForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Get values from the form
+    const name = document.getElementById("name").value;
+    const imageUrl = document.getElementById("imageUrl").value;
+    const status = document.getElementById("status").value;
+    const breed = document.getElementById("breed").value;
+    const age = document.getElementById("age").value;
+
+    // Create a new player object
+    const newPlayer = {
+        name,
+        imageUrl,
+        status,
+        breed,
+        age: parseInt(age),
+    };
+
+    // Add the new player to the state
+    state.allPlayers.push(newPlayer);
+
+    // Clear the form inputs
+    document.getElementById("playerForm").reset();
+
+    // Render the updated player list
+    renderPlayers();
+});
+
+
+
+
 
 
 
